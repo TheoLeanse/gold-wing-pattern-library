9eca2c53591f6f059189ad74d37d267cedbec296
theoleanse <theoleanse@gmail.com>
2018-05-25 14:08:34 +0100

    show patterns

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
 import React from 'react';
 
-export default () => <div>Gold Wing pattern library</div>;
+export default ({ data }) => (
+	<div>
+		<div>Gold Wing pattern library</div>
+		{data.allMarkdownRemark.edges.map(edge => (
+			<div>
+				<div>{edge.node.frontmatter.title}</div>
+				<div dangerouslySetInnerHTML={{ __html: edge.node.html }} />
+			</div>
+		))}
+	</div>
+);
+
+export const pageQuery = graphql`
+	query AllPatterns {
+		allMarkdownRemark {
+			edges {
+				node {
+					id
+					html
+					frontmatter {
+						title
+						path
+						date
+					}
+				}
+			}
+		}
+	}
+`;
